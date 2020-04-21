fe3f9a9fc1aa48738df12dee8122c541c1f13ac6
Ashwin Kumar <ashwinkumar9830@gmail.com>
2020-04-21 12:51:04 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -94,6 +94,7 @@ class Game extends React.Component {
         'Go to move #' + move :
         'Go to game start';
       return (
+
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
@@ -108,18 +109,25 @@ class Game extends React.Component {
     }
 
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={i => this.handleClick(i)}
-          />
+      <div>
+        <div className="hero">
+          <h1>Tic-Tac-Toe</h1>
+          <div className="game-info">
+            <div>{status}</div>
+            {/* <ol>{moves}</ol> */}
+          </div>
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+        <div className="game">
+
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={i => this.handleClick(i)}
+            />
+          </div>
         </div>
       </div>
+
     );
   }
 }
